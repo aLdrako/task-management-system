2563a54beb76161dce0b393a101e07c733655966
LaptopARO <petrov.argir225@gmail.com>
2022-12-26 17:46:21 +0200

    ADDING A METHOD TO CHECK IF IT'S THE SAME AS A TEAM/USER

--- Skeleton/src/com/telerikacademy/tms/commands/CreateBoardInTeam.java
+++ Skeleton/src/com/telerikacademy/tms/commands/CreateBoardInTeam.java
@@ -2,6 +2,8 @@ package com.telerikacademy.tms.commands;
 
 import com.telerikacademy.tms.commands.contracts.Command;
 import com.telerikacademy.tms.core.contracts.TaskManagementRepository;
+import com.telerikacademy.tms.exceptions.DuplicateElementException;
+import com.telerikacademy.tms.exceptions.InvalidUserInputException;
 import com.telerikacademy.tms.models.contracts.Board;
 import com.telerikacademy.tms.models.contracts.Team;
 import com.telerikacademy.tms.utils.ValidationHelpers;
@@ -11,6 +13,7 @@ import java.util.List;
 public class CreateBoardInTeam implements Command {
 	public static final int EXPECTED_NUMBER_PARAMETERS = 2;
 	public static final String BOARD_CREATED_SUCCESSFULLY = "Board %s has been created in team %s!";
+	public static final String DUPLICATE_NAME_MESSAGE = "Duplicate name. There is a User/Team with the same name!";
 	private final TaskManagementRepository repository;
 
 	public CreateBoardInTeam(TaskManagementRepository repository) {
@@ -20,9 +23,17 @@ public class CreateBoardInTeam implements Command {
 	@Override
 	public String execute(List<String> parameters) {
 		ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_PARAMETERS);
-		String name = parameters.get(0);
+		String name = isNameSameAsUserOrTeam(parameters.get(0));
 		String team = parameters.get(1);
 		return createBoardInTeam(name, team);
+	}
+
+	private String isNameSameAsUserOrTeam(String name) {
+		if (repository.getUsers().stream().anyMatch(user -> user.getName().equalsIgnoreCase(name)) ||
+				repository.getTeams().stream().anyMatch(team -> team.getName().equalsIgnoreCase(name))) {
+		throw new DuplicateElementException(DUPLICATE_NAME_MESSAGE);
+		}
+		return name;
 	}
 
 	private String createBoardInTeam(String boardName, String teamName) {
