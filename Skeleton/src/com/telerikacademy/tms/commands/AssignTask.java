802587b578c62856c3695384e181d5f6f110b9bb
LaptopARO <petrov.argir225@gmail.com>
2022-12-27 21:15:23 +0200

    Fixing the AssignTask method

--- Skeleton/src/com/telerikacademy/tms/commands/AssignTask.java
+++ Skeleton/src/com/telerikacademy/tms/commands/AssignTask.java
@@ -3,6 +3,10 @@ package com.telerikacademy.tms.commands;
 import com.telerikacademy.tms.commands.contracts.Command;
 import com.telerikacademy.tms.core.contracts.TaskManagementRepository;
 import com.telerikacademy.tms.exceptions.ElementNotFoundException;
+import com.telerikacademy.tms.exceptions.InvalidUserInputException;
+import com.telerikacademy.tms.models.TeamImpl;
+import com.telerikacademy.tms.models.contracts.Board;
+import com.telerikacademy.tms.models.contracts.Team;
 import com.telerikacademy.tms.models.contracts.User;
 import com.telerikacademy.tms.models.tasks.contracts.Assignable;
 import com.telerikacademy.tms.models.tasks.contracts.Task;
@@ -16,6 +20,9 @@ import static java.lang.String.format;
 
 public class AssignTask implements Command {
 	private static final String TASK_ASSIGNED_SUCCESSFUL = "Task with ID %s was assigned to user %s.";
+	private static final String USER_NOT_FOUND_IN_TEAM = "Cannot find user in team in order to assign task";
+	private static final String TASK_ALREADY_ASSIGNED = "Task is already assigned to %s!";
+
 	private static final int EXPECTED_NUMBER_PARAMETERS = 2;
 	private final TaskManagementRepository repository;
 
@@ -37,8 +44,20 @@ public class AssignTask implements Command {
 		try {
 			assignableTask = (Assignable) repository.findElementById(repository.getTasks(), id);
 			User user = repository.findElementByName(repository.getUsers(), userName);
-			user.assignTask((Task) assignableTask);
-			assignableTask.setAssignee(user);
+
+			Team team = repository.getTeams().stream()
+					.filter(team1 -> team1.getUsers().contains(user))
+					.findFirst()
+					.orElseThrow(() -> new InvalidUserInputException(USER_NOT_FOUND_IN_TEAM));
+
+			if (team.getBoards().stream().anyMatch(board -> board.getTasks().stream().anyMatch(task1 -> task1.getID() == id))) {
+				if (!assignableTask.getAssignee().getName().equalsIgnoreCase("Unassigned")) {
+					throw new InvalidUserInputException(format(TASK_ALREADY_ASSIGNED, assignableTask.getAssignee().getName()));
+				}
+				user.assignTask((Task) assignableTask);
+				assignableTask.setAssignee(user);
+			}
+
 		} catch (ClassCastException e) {
 			throw new ClassCastException(format(INVALID_TASK_ID_IN_CATEGORY, id, Assignable.class.getSimpleName()));
 		} catch (ElementNotFoundException e) {
