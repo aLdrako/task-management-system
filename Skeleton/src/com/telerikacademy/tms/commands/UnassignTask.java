a3289c57c23f4d51a2374bd3a02fc25f2beca500
LaptopARO <petrov.argir225@gmail.com>
2022-12-28 10:11:51 +0200

    UnassignTask command will only accept 1 parameter which is the ID of the task. There is no reason give the name of the user as well as a second parameter since a task can only be assigned to ONE person.

--- Skeleton/src/com/telerikacademy/tms/commands/UnassignTask.java
+++ Skeleton/src/com/telerikacademy/tms/commands/UnassignTask.java
@@ -3,10 +3,10 @@ package com.telerikacademy.tms.commands;
 import com.telerikacademy.tms.commands.contracts.Command;
 import com.telerikacademy.tms.core.contracts.TaskManagementRepository;
 import com.telerikacademy.tms.exceptions.ElementNotFoundException;
+import com.telerikacademy.tms.exceptions.InvalidUserInputException;
 import com.telerikacademy.tms.models.UserImpl;
 import com.telerikacademy.tms.models.contracts.User;
 import com.telerikacademy.tms.models.tasks.contracts.Assignable;
-import com.telerikacademy.tms.models.tasks.contracts.Task;
 
 import java.util.List;
 
@@ -17,7 +17,9 @@ import static java.lang.String.format;
 
 public class UnassignTask implements Command {
 	private static final String TASK_UNASSIGNED_SUCCESSFUL = "Task with ID -> [%s] was unassigned from user <%s>.";
-	private static final int EXPECTED_NUMBER_PARAMETERS = 2;
+	private static final String TASK_ALREADY_UNASSIGNED = "Task with ID -> [%s] is already unassigned.";
+
+	private static final int EXPECTED_NUMBER_PARAMETERS = 1;
 	private final TaskManagementRepository repository;
 
 	public UnassignTask(TaskManagementRepository repository) {
@@ -28,17 +30,19 @@ public class UnassignTask implements Command {
 	public String execute(List<String> parameters) {
 		validateArgumentsCount(parameters, EXPECTED_NUMBER_PARAMETERS);
 		int id = tryParseInt(parameters.get(0));
-		String userName = parameters.get(1);
 
-		return unassignTask(id, userName);
+		return unassignTask(id);
 	}
 
-	private String unassignTask(int id, String userName) {
+	private String unassignTask(int id) {
 		Assignable assignableTask;
+		User previousAssignee;
 		try {
 			assignableTask = (Assignable) repository.findElementById(repository.getTasks(), id);
-			User user = repository.findElementByName(repository.getUsers(), userName);
-			user.unAssignTask((Task) assignableTask);
+			if (assignableTask.getAssignee().getName().equalsIgnoreCase("Unassigned")) {
+				throw new InvalidUserInputException(format(TASK_ALREADY_UNASSIGNED, assignableTask.getID()));
+			}
+			previousAssignee = assignableTask.getAssignee();
 			assignableTask.setAssignee(new UserImpl("Unassigned"));
 		} catch (ClassCastException e) {
 			throw new ClassCastException(format(INVALID_TASK_ID_IN_CATEGORY, id, Assignable.class.getSimpleName()));
@@ -46,6 +50,6 @@ public class UnassignTask implements Command {
 			throw new ElementNotFoundException(e.getMessage());
 		}
 
-		return format(TASK_UNASSIGNED_SUCCESSFUL, id, userName);
+		return format(TASK_UNASSIGNED_SUCCESSFUL, id, previousAssignee.getName());
 	}
 }
